15cdf1ffb3f96eebad672b458bd45393cccf279a
ndgh1606 <muigiahuy1606@gmail.com>
2023-11-28 13:29:54 +0700

    Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React from "react"
 import "./Hero.css"
+import {HiLocationMarker} from 'react-icons/hi'
+import CountUp from "react-countup"
 const Hero = () =>{
     return (
         <section className="hero-wrapper">
                 <div className="paddings innerWidth flexCenter hero-container">
-                    <div className="hero-left">
+                <div className="flexColStart hero-left">
                         <div className="hero-title">
+                            <div className="orange-circle"/>
                             <h1>
-                            Discover<br/> Most Suitable<br/> Property
+                            Discover<br/> 
+                            Most Suitable<br/> Property
                             </h1>
                         </div>
 
@@ -16,9 +20,43 @@ const Hero = () =>{
                         <span>Forget all difficulties in finding a residence for you</span>
                     </div>
 
-                    <div className="search-bar">
-                        search bar
+                    <div className="flexCenter search-bar">
+                        <HiLocationMarker color="var(--blue)" size={25}/>
+                        <input type='text'/>
+                        <button className="button">Search</button>
                     </div>
+
+                    <div className="flexCenter stats">
+                        <div className="flexColStart stat">
+                            <span>
+                                <CountUp start={8800} end={9000} duration={4}/>
+                                <span>+</span>
+                                <span>
+                                Premium Product
+                                </span>
+                            </span>
+                        </div>
+                        <div className="flexColStart stat">
+                            <span>
+                                <CountUp start={1950} end={2000} duration={4}/>
+                                <span>+</span>
+                                <span>
+                                Premium Product
+                                </span>
+                            </span>
+                        </div>
+                        <div className="flexColStart stat">
+                            <span>
+                                <CountUp end={28}/>
+                                <span>+</span>
+                                <span>
+                                Premium Product
+                                </span>
+                            </span>
+                        </div>
+                        
+                    </div>
+                    
                     </div>
                 {/* right site */}
                     <div className="flexCenter hero-right">
